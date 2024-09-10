9a70f8dbea4c82c3d345e19aba55067dd8f9831e
Damien Dumontet <damien.dumontet@protonmail.com>
2024-09-10 19:33:12 -0300

    create button animation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
 import "./style.css";
+import anime from "animejs/lib/anime.es.js";
 
-console.log("WORKING");
+document
+  .querySelector(".connect_btn_wrapper")
+  ?.addEventListener("mouseenter", () => {
+    anime({
+      targets: ".connect_btn_transition",
+      scale: 2000,
+      easing: "easeOutElastic(1, 0.5)",
+    });
+  });
+
+document
+  .querySelector(".connect_btn_wrapper")
+  ?.addEventListener("mouseleave", () => {
+    anime({
+      targets: ".connect_btn_transition",
+      scale: 0,
+      easing: "easeInElastic(1, 0.5)",
+    });
+  });
