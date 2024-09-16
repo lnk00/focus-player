35fd91c49412c130b34084bb95132a42dbaa5e9d
Damien Dumontet <damien.dumontet@protonmail.com>
2024-09-16 18:55:58 -0300

    add spotify endpoint

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,11 +1,11 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import spotify from "./spotify";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.get("/", (c) => c.text("Focus player api v1.0.0"));
+app.route("/spotify", spotify);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
